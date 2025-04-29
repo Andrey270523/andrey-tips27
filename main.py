8c3e83962d7029faf425789de2cc5a0c2d247042
Andrey270523 <sandreysantos03@gmail.com>
2025-04-29 05:06:45 -0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,7 @@
+# main.py
+
+from telegram.notifier import send_telegram_alert
+
+if __name__ == '__main__':
+    print('Robô de apostas iniciado...')
+    # Exemplo: send_telegram_alert('Robô está rodando!')
